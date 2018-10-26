1d1d36ced58a3b2a1216d6c4a3cd8db376d82cea
zsomborvermes <zv222ae@student.lnu.se>
2018-10-26 17:26:38 +0200

    add define-basic-info

---
+++ week-02/day-1/define-basic-info.ts
@@ -0,0 +1,20 @@
+
+'use strict';
+export{};
+
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+let name: string = "Vermes Zsombor";
+let age: number = 23;
+let height: number = 1.66;
+let married: boolean = false;
+
+console.log(name);
+console.log(age);
+console.log(height);
+console.log(married);
