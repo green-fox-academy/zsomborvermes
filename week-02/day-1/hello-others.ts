e4ca03b128edf7d828e89d861f3515f724c3a9f2
zsomborvermes <zv222ae@student.lnu.se>
2018-10-26 16:33:48 +0200

    add hello-others.ts

---
+++ week-02/day-1/hello-others.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+// Greet 3 of your classmates with this program, in three separate lines like:
+//
+// Hello, Esther!
+// Hello, Mary!
+// Hello, Joe!
+
+console.log("Hello, Gábor!");
+console.log("Hello, Laura!");
+console.log("Hello, Dani!");
