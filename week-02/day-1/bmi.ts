3ca447c59f39cec525de63776a4286479b6c9afa
zsomborvermes <zv222ae@student.lnu.se>
2018-10-26 17:12:07 +0200

    add bmi.ts

---
+++ week-02/day-1/bmi.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+let massInKg: number = 81.2;
+let heightInM: number = 1.78;
+
+console.log((massInKg/heightInM)/heightInM);
+
+// Print the Body mass index (BMI) based on these values
