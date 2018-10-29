4e40cbff05aa9f18612ed2d27c038bc7df66962a
zsomborvermes <zv222ae@student.lnu.se>
2018-10-29 13:49:21 +0100

    add draw-square.ts

---
+++ week-02/day-1/draw-square.ts
@@ -0,0 +1,34 @@
+'use strict';
+export {};
+
+let lineCount: number = 6;
+
+// Write a program that draws a square like this:
+//
+// %%%%%%
+// %    %
+// %    %
+// %    %
+// %    %
+// %%%%%%
+//
+// The square should have as many lines as lineCount is
+
+for (let i: number = 0; i < lineCount; i++){
+    let currentLine: string = "";
+    if (i == 0 || i == lineCount-1){
+        for (let j: number = 1; j <= lineCount; j++){
+            currentLine = currentLine + "%";
+        }
+        console.log(currentLine);
+    } else{
+        for (let l: number = 1; l <= lineCount; l++){
+            if (l == 1 || l == lineCount){
+                currentLine = currentLine + "%";
+            } else {
+                currentLine = currentLine + " ";
+            }
+        }
+        console.log(currentLine);
+    }
+}
