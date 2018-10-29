d15b1e12ec5fdcd07b08384f0a6bd763888d9793
zsomborvermes <zv222ae@student.lnu.se>
2018-10-29 16:44:19 +0100

    add parametric-average.ts

---
+++ week-02/day-1/parametric-average.ts
@@ -0,0 +1,32 @@
+'use strict';
+
+// Write a program that asks for a number.
+// It would ask this many times to enter an number,
+// if all the numbers are entered, it should print the sum and average of these
+// numbers like:
+//
+// Sum: 22, Average: 4.4
+
+console.log("How many numbers will you enter?: ");
+let input: any = process.openStdin();
+let sum: number = 0;
+let inputCounter: number = 0;
+let NumberOfInputs = 0;
+
+
+input.addListener("data", function (d) {
+    if (inputCounter == 0) {
+        NumberOfInputs = +d.toString().trim();
+        console.log("Enter a number! ");
+    } else if (inputCounter < NumberOfInputs) {
+        console.log("Enter a number! ");
+        sum += +d.toString().trim();
+    } else {
+        console.log("Enter a number! ");
+        sum += parseInt(d.toString().trim());
+        console.log("Sum: " + sum + ", Average: " + sum / NumberOfInputs);
+        process.exit();
+    }
+    inputCounter++;
+
+});
