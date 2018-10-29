076abc1ee97c655cc232ef0da4d88fce4ba8740a
zsomborvermes <zv222ae@student.lnu.se>
2018-10-29 11:06:52 +0100

    declared variable type in for loops for variable i

--- week-02/day-1/i-wont-cheat-on-the-exams.ts
+++ week-02/day-1/i-wont-cheat-on-the-exams.ts
@@ -3,6 +3,6 @@
 // Create a program that writes this line 100 times:
 // "I won't cheat on the exam!"
 
-for( let i = 0; i < 100; i++){
+for( let i: number = 0; i < 100; i++){
     console.log("I won't cheat on the exam!")
 }
