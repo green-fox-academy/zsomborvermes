9a579a6a5732c368f575c0c6d2c9fbd274a362c8
zsomborvermes <zv222ae@student.lnu.se>
2018-10-30 10:27:15 +0100

    add append-a-func.ts

---
+++ week-02/day-2/append-a-func.ts
@@ -0,0 +1,16 @@
+'use strict';
+export{};
+
+// -  Create a string variable named `typo` and assign the value `Chinchill` to it
+// -  Write a function called `appendAFunc` that gets a string as an input,
+//    appends an 'a' character to its end and returns with a string
+// -  Print the result of `appendAFunc(typo)`
+
+let typo: string = "Chinchill";
+
+function appendAFunc(input: string){
+    input += 'a';
+    return input;
+}
+
+console.log(appendAFunc(typo));
