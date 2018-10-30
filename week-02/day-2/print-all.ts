d8622c9407d84ed286c3390e516a1b8b978058c4
zsomborvermes <zv222ae@student.lnu.se>
2018-10-30 11:30:31 +0100

    add print-all.ts

---
+++ week-02/day-2/print-all.ts
@@ -0,0 +1,11 @@
+'use strict';
+export{};
+
+// -  Create a variable named `integers` with the following content: `[4, 5, 6, 7]`
+// -  Log each the element of `integers` to the console*
+// *hint: use a loop, console.log(integers) won't cut it
+// -  bonus for using the correct built in array method
+
+let integers: number[] = [4, 5, 6, 7];
+
+integers.map(e => console.log(e));
