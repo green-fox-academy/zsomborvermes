57e33455d7984eefe4b17a0d7868a764b9968963
zsomborvermes <zv222ae@student.lnu.se>
2018-10-30 11:04:12 +0100

    add third.ts

---
+++ week-02/day-2/third.ts
@@ -0,0 +1,10 @@
+'use strict';
+export{};
+
+// -  Create a variable named `magicNumbers`
+//    with the following content: `[1, 3, 5, 7]`
+// -  Print the third element of `magicNumbers`
+
+let magicNumbers: number[] = [1, 3, 4, 7];
+
+console.log(magicNumbers[2]);
