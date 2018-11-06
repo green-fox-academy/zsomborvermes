0adf922cc1036c38e966be4b48428070614adda9
zsomborvermes <zv222ae@student.lnu.se>
2018-11-06 11:13:29 +0100

    corrected syntax

--- week-03/reverse.ts
+++ week-03/reverse.ts
@@ -4,8 +4,8 @@
 
 let reversed: string = '.eslaf eb t\'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI';
 
-function reverse(str: string): string{
-return str.split("").reverse().join("")
+function reverse(str: string): string {
+  return str.split("").reverse().join("");
 }
 
 console.log(reverse(reversed));
