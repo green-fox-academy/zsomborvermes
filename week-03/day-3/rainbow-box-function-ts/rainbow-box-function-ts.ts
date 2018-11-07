8c48b722787629230ba1394b2d524f853036f825
zsomborvermes <zv222ae@student.lnu.se>
2018-11-07 11:58:31 +0100

    add rainbow-box-function-ts

---
+++ week-03/day-3/rainbow-box-function-ts/rainbow-box-function-ts.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Create a square drawing function that takes 2 parameters:
+// The square size, and the fill color,
+// and draws a square of that size and color to the center of the canvas.
+// Create a loop that fills the canvas with rainbow colored squares.
+
+let colorsOfRainbow: string[] = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+function drawColoredSquare(size: number, color: string) {
+  ctx.fillStyle = color;
+  ctx.fillRect(canvas.width / 2 - size / 2, canvas.height / 2 - size / 2, size, size);
+}
+
+for (let i: number = 8; i > 0; i--) {
+  drawColoredSquare(i * 100, colorsOfRainbow[i]);
+}
