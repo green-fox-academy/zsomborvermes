268b36d2a0716a619c0cf31689bf02a0b3ec5411
zsomborvermes <zv222ae@student.lnu.se>
2018-11-07 12:11:12 +0100

    add purple-steps-ts

---
+++ week-03/day-3/purple-steps-ts/purple-steps-ts.ts
@@ -0,0 +1,20 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Reproduce this:
+// [https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps/r3.png]
+
+function drawPurpleSquare(x: number, y: number) {
+  ctx.fillStyle = 'purple';
+  ctx.fillRect(x, y, 15, 15);
+  ctx.strokeStyle = 'black';
+  ctx.strokeRect(x, y, 15, 15);
+}
+
+for (let i: number = 1; i <= 20; i++){
+  drawPurpleSquare(i*15, i*15);
+}
