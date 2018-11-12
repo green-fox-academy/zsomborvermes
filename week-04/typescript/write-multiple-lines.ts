42455ee17bfea1a93280627e88d905c46a07aabd
zsomborvermes <zv222ae@student.lnu.se>
2018-11-12 10:11:42 +0100

    add write-multiple-lines

---
+++ week-04/typescript/write-multiple-lines.ts
@@ -0,0 +1,21 @@
+export { };
+// Create a function that takes 3 parameters: a path, a word and a number,
+// than it should write to a file.
+// The path parameter should be a string, that describes the location of the file.
+// The word parameter should be a string, that will be written to the file as lines
+// The number paramter should describe how many lines the file should have.
+// So if the word is 'apple' and the number is 5, than it should write 5 lines
+// to the file and each line should be 'apple'
+// The function should not raise any error if it could not write the file. 
+
+let fs = require('fs');
+
+function writeLines(path: string, word: string, numOfLines: number) {
+  let out: string = ""
+  for (let i: number = 0; i < numOfLines; i++) {
+    out += word + '\n';
+  }
+  fs.writeFileSync(path, out);
+}
+
+writeLines('apple.txt', 'apple', 5);
