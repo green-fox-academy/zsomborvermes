16c89ae88bce2d9d68780df9857ba12c538c013a
zsomborvermes <zv222ae@student.lnu.se>
2018-11-12 10:16:43 +0100

    file management

---
+++ week-04/typescript/day-1/count-lines.ts
@@ -0,0 +1,22 @@
+export { };
+// Write a function that takes a filename as string,
+// then returns the number of lines the file contains.
+// It should return zero if it can't open the file, and
+// should not raise any error.
+
+const fs = require('fs');
+
+function countLines(filename: string): number {
+  let numberOfLines: number = 0;
+  try {
+    let fileContent = fs.readFileSync(filename, 'utf-8');
+    numberOfLines = fileContent.split("\n").length;
+    return numberOfLines;
+
+  } catch (err) {
+    return numberOfLines;
+  }
+}
+
+console.log(countLines('files/message.txt'));
+console.log(countLines('mess.txt'));
