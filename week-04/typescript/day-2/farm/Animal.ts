e06708c278f4903bf840bd8e552e3e83b8546f3c
zsomborvermes <zv222ae@student.lnu.se>
2018-11-13 15:25:45 +0100

    add farm

---
+++ week-04/typescript/day-2/farm/Animal.ts
@@ -0,0 +1,28 @@
+export class Animal {
+  private hunger: number;
+  private thirst: number;
+
+  constructor() {
+    this.hunger = 50;
+    this.thirst = 50;
+  }
+
+  eat() {
+    this.hunger--;
+    console.log('Hunger: ' + this.hunger);
+  }
+
+  drink() {
+    this.thirst--;
+    console.log('Thirst: ' + this.thirst);
+  }
+
+  play() {
+    this.hunger++;
+    this.thirst++;
+    console.log('Hunger and thirst: ' + this.hunger + ', ' + this.thirst);
+  }
+  getHunger() {
+    return this.hunger;
+  }
+}
---
+++ week-04/typescript/day-2/farm/Farm.ts
@@ -0,0 +1,45 @@
+import { Animal } from "./Animal";
+
+export class Farm {
+  private animalList: Animal[] = [];
+  private slots: number = 10;
+
+  breed() {
+    if (this.animalList.length < 10) {
+      this.animalList.push(new Animal());
+    }
+  }
+
+  slaughter() {
+    let leastHungry: Animal;
+    for (let i = 0; i < this.animalList.length; i++) {
+      if (i === 0) {
+        leastHungry = this.animalList[i];
+      } else if (this.animalList[i].getHunger() < leastHungry.getHunger()) {
+        leastHungry = this.animalList[i];
+      }
+    }
+    this.animalList.splice(this.animalList.indexOf(leastHungry), 1);
+  }
+
+  getAnimalList() {
+    return this.animalList;
+  }
+}
+
+let farm = new Farm();
+
+for (let i = 0; i < 20; i++) {
+  farm.breed();
+}
+
+for (let i = 0; i < 200; i++) {
+  farm.getAnimalList()[Math.floor(Math.random() * 10)].play();
+}
+console.log(farm);
+
+farm.slaughter();
+farm.slaughter();
+farm.slaughter();
+console.log(farm);
+
