b11de771b95ae5a695318a843119bbdfbe208ba0
zsomborvermes <zv222ae@student.lnu.se>
2018-11-13 10:22:27 +0100

    add pokemon

---
+++ week-04/typescript/day-2/pokemon/main.ts
@@ -0,0 +1,30 @@
+import { Pokemon } from './Pokemon'
+
+let pokemonOfAsh: Pokemon[] = initializePokemon();
+
+// Every pokemon has a name and a type.
+// Certain types are effective against others, e.g. water is effective against fire.
+
+// Ash has a few pokemon.
+// A wild pokemon appeared!
+
+let wildPokemon: Pokemon = new Pokemon('Oddish', 'leaf', 'water');
+
+// Which pokemon should Ash use?
+
+pokemonOfAsh.forEach(e => {
+  if (e.isEffectiveAgainst(wildPokemon)) {
+    console.log('I choose you, ' + e.name);
+  }
+})
+
+
+function initializePokemon(): Pokemon[] {
+  return [
+    new Pokemon('Balbasaur', 'leaf', 'water'),
+    new Pokemon('Pikatchu', 'electric', 'water'),
+    new Pokemon('Charizard', 'fire', 'leaf'),
+    new Pokemon('Balbasaur', 'water', 'fire'),
+    new Pokemon('Kingler', 'water', 'fire')
+  ];
+}
