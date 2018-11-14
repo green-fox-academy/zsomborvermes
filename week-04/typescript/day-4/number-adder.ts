c0948c3f0f5313df61ebd0c9c0079bda0352953f
zsomborvermes <zv222ae@student.lnu.se>
2018-11-14 22:19:45 +0100

    number-adder.ts

--- week-04/typescript/day-4/number-adder.ts
+++ week-04/typescript/day-4/number-adder.ts
@@ -2,9 +2,8 @@
 function numberAdder(n: number): number {
   if (n === 1) {
     return 1;
-  } else {
-    return n + numberAdder(n - 1);
   }
+  return n + numberAdder(n - 1);
 }
 
 console.log(numberAdder(10));
