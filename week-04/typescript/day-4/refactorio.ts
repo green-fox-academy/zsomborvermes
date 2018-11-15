3629c1cad6dfadfb74819b7abfdff3cca7235104
zsomborvermes <zv222ae@student.lnu.se>
2018-11-15 10:37:32 +0100

    add refactorio.ts

--- week-04/typescript/day-4/refactorio.ts
+++ week-04/typescript/day-4/refactorio.ts
@@ -3,10 +3,9 @@ function refactorio(num: number): number {
   if (num == 1) {
     return 1;
 
-  } else {
-    return num * (refactorio(num - 1));
-    
   }
+
+  return num * (refactorio(num - 1));
 }
 
 console.log(refactorio(5));
