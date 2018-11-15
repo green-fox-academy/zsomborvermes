d4a68e1b118c5b566a30e8eeb82b336d09d88d09
zsomborvermes <zv222ae@student.lnu.se>
2018-11-15 11:14:19 +0100

    changed function name

--- week-04/typescript/day-4/bunny1.ts
+++ week-04/typescript/day-4/bunny1.ts
@@ -1,12 +1,12 @@
 
 
-function countBunnies(bunnies: number): number {
+function countBunnyEars(bunnies: number): number {
   if (bunnies === 1) {
     return 2;
   }
-  return countBunnies(1) + countBunnies(bunnies - 1);
+  return countBunnyEars(1) + countBunnyEars(bunnies - 1);
 }
 
-console.log(countBunnies(400));
+console.log(countBunnyEars(400));
 
 
