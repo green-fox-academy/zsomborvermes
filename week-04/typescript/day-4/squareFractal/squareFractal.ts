838c3db3e644194f19b205abea5ee76ef7468b6e
zsomborvermes <zv222ae@student.lnu.se>
2018-11-15 15:08:04 +0100

    add squareFractal

---
+++ week-04/typescript/day-4/squareFractal/squareFractal.ts
@@ -0,0 +1,28 @@
+'use strict'
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+function drawPattern(x: number, y: number, size: number, depth: number): void {
+  for (let i = 1; i < 3; i++) {
+    ctx.beginPath();
+    ctx.moveTo(x + size / 3 * i, y);
+    ctx.lineTo(x + size / 3 * i, y + size);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(x, y + size / 3 * i);
+    ctx.lineTo(x + size, y + size / 3 * i);
+    ctx.stroke();
+  }
+
+  if (depth > 1) {
+    drawPattern(x, y + size / 3, size / 3, depth - 1);
+    drawPattern(x + size / 3, y, size / 3, depth - 1);
+    drawPattern(x + size * 2 / 3, y + size / 3, size / 3, depth - 1);
+    drawPattern(x + size / 3, y + size * 2 / 3, size / 3, depth - 1);
+  }
+}
+
+
+drawPattern(0, 0, canvas.width, 6);
