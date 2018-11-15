4fbff0f57e6457e11534d0c20d0f6897b7b1755c
zsomborvermes <zv222ae@student.lnu.se>
2018-11-15 16:27:06 +0100

    add hexaHexaFractal

---
+++ week-04/typescript/day-4/hexaHexaFractal/hexaHexaFractal.ts
@@ -0,0 +1,26 @@
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+function drawHexagonFractal(x: number, y: number, size: number, depth: number): void {
+  
+  ctx.beginPath();
+  ctx.moveTo(x + size * Math.cos(0), y + size * Math.sin(0));
+  for (let i = 0; i < 7; i++) {
+    ctx.lineTo(x + size * Math.cos(i * 2 * Math.PI / 6), y + size * Math.sin(i * 2 * Math.PI / 6));
+  }
+
+  ctx.stroke();
+  ctx.closePath();
+  if (depth > 1) {
+    drawHexagonFractal(x + size / 3, y - size / 1.73, size / 3, depth - 1);
+    drawHexagonFractal(x - size / 3, y - size / 1.73, size / 3, depth - 1);
+    drawHexagonFractal(x - size / 3, y + size / 1.73, size / 3, depth - 1);
+    drawHexagonFractal(x + size / 3, y + size / 1.73, size / 3, depth - 1);
+    drawHexagonFractal(x + size / 1.5, y, size / 3, depth - 1);
+    drawHexagonFractal(x - size / 1.5, y, size / 3, depth - 1);
+
+  }
+
+}
+
+drawHexagonFractal(250, 250, 250, parseInt(prompt('Please set the deapth of the fractal: ')));
