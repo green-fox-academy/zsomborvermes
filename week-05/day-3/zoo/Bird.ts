8f14bfcf95456bb7a924275d90f94877426764e6
zsomborvermes <zv222ae@student.lnu.se>
2018-11-21 12:29:44 +0100

    feal(zoo): implemented flyable interface in Bird class

--- week-05/day-3/zoo/Bird.ts
+++ week-05/day-3/zoo/Bird.ts
@@ -1,11 +1,23 @@
 import { Animal } from "./Animal";
 
-export class Bird extends Animal {
+export class Bird extends Animal implements Flyable{
   constructor(name: string, age?: number) {
     super(name, age);
   }
 
   breed(): string {
     return 'laying eggs';
+  }
+
+  land() {
+    console.log(`${this.constructor.name} has landed successfully.`);
+  }
+
+  fly() {
+    console.log(`${this.constructor.name} is flying.`);
+  }
+
+  takeOff() {
+    console.log(`${this.constructor.name} has taken off succesfully`);
   }
 }
