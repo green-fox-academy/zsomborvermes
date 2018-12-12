749532eb500fc48bc019ed8bb5136b943a0e4e7a
zsomborvermes <zv222ae@student.lnu.se>
2018-12-12 12:11:48 +0100

    feat(BankAccountController): added endpoint for HTMLCeption

--- week-07/day-3/bankOfSimba/src/main/java/com/greenfoxacademy/bankOfSimba/BankAccountController.java
+++ week-07/day-3/bankOfSimba/src/main/java/com/greenfoxacademy/bankOfSimba/BankAccountController.java
@@ -16,4 +16,12 @@ public class BankAccountController {
         model.addAttribute("simba", simba);
         return "show";
     }
+
+
+    @RequestMapping(path = "/htmlception", method = RequestMethod.GET)
+    public String htmlCeption(Model model) {
+
+        model.addAttribute("text", "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>");
+        return "htmlception";
+    }
 }
