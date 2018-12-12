abfad869a374a4e85852538d86aad605f9c25735
zsomborvermes <zv222ae@student.lnu.se>
2018-12-12 11:04:32 +0100

    feat(BankAccount): created fields and constructor

--- week-07/day-3/bankOfSimba/src/main/java/com/greenfoxacademy/bankOfSimba/BankAccount.java
+++ week-07/day-3/bankOfSimba/src/main/java/com/greenfoxacademy/bankOfSimba/BankAccount.java
@@ -1,4 +1,13 @@
 package com.greenfoxacademy.bankOfSimba;
 
 public class BankAccount {
+    private String name;
+    private double balance;
+    private String animalType;
+
+    public BankAccount(String name, double balance, String animalType) {
+        this.name = name;
+        this.balance = balance;
+        this.animalType = animalType;
+    }
 }
