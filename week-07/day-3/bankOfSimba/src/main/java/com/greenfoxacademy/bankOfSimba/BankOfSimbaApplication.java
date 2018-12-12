ba5828a684f657ca401dd6c0db992c542caf9e1d
zsomborvermes <zv222ae@student.lnu.se>
2018-12-12 10:56:16 +0100

    feat(bankofsimba): created project

---
+++ week-07/day-3/bankOfSimba/src/main/java/com/greenfoxacademy/bankOfSimba/BankOfSimbaApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.bankOfSimba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankOfSimbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankOfSimbaApplication.class, args);
+	}
+}
